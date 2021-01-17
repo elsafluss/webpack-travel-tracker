6728ae3347842466acac0335ecb6970d9ba7974e
Elsa Fluss <elsafluss@gmail.com>
2021-01-17 15:02:18 -0700

    Create tests for Traveler class, all pass.

--- src/traveler.js
+++ src/traveler.js
@@ -1,21 +1,46 @@
-/* eslint-disable max-len */
 class Traveler {
-  constructor(travelerID, travelerName, travelerType,
-    trips, destinations, totalSpent) {
-    this.travelerID = travelerID,
-    this.travelerName = travelerName,
-    this.travelerType = travelerType,
-    this.trips = trips,
-    this.destinations = destinations,
-    this.totalSpent = totalSpent
+  constructor(
+    travelerID,
+    travelerName,
+    travelerType,
+    trips,
+    destinations,
+    totalSpent
+  ) {
+    (this.travelerID = travelerID),
+    (this.travelerName = travelerName),
+    (this.travelerType = travelerType),
+    (this.trips = trips),
+    (this.destinations = destinations),
+    (this.totalSpent = totalSpent),
+    this.futureTrips = [],
+    this.pastTrips = []
   }
 
-  sortMyTrips(type, userID) { // return pending or approved trips
-    let tripsData = this.trips
-      .filter((trips) => {
-        return (trips.userID === userID) && (trips.status === type)
-      })
+  getMyTrips(userID) {
+    let myTrips = this.trips.filter((trips) => {
+      return trips.userID === userID
+    })
+    return myTrips
+  }
+
+  sortMyTrips(type, userID) {
+    let tripsData = this.trips.filter((trips) => {
+      return trips.userID === userID && trips.status === type
+    })
     return tripsData
+  }
+
+  catalogueTrips(userID) {
+    let myTrips = this.getMyTrips(userID)
+    let today = Date.now()
+    myTrips.filter(trip => {
+      if (new Date(trip.date) > new Date(today)) {
+        this.futureTrips.push(trip)
+      } else {
+        this.pastTrips.push(trip)
+      }
+    })
   }
 
   sortTripsByDate(date, userID) {
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,74 +1,80 @@
-/* eslint-disable max-len */
 import chai from "chai"
 const expect = chai.expect
-import Traveler, { sortMyTrips } from '../src/traveler'
+import Traveler from '../src/traveler'
 
 describe("Traveler", () => {
 
   let traveler1,
-    traveler2,
     travelerID1,
-    travelerID2,
     traveler1Name,
-    traveler2Name,
     traveler1Type,
-    traveler2Type,
     trips,
     destinations,
-    totalSpent1,
-    totalSpent2
+    totalSpent1
 
   beforeEach(() => {
     (travelerID1 = 1),
-    (travelerID2 = 2),
     (traveler1Name = "Wesley Crusher"),
-    (traveler2Name = "Marco Polo"),
     (traveler1Type = "exotic"),
-    (traveler2Type = "spicy"),
-    (trips = [
-      {
-        id: 44,
-        userID: 1,
-        destinationID: 49,
-        travelers: 1,
-        date: "2019/09/16",
-        duration: 8,
-        status: "approved",
-        suggestedActivities: [],
-      },
-      {
-        id: 35,
-        userID: 2,
-        destinationID: 25,
-        travelers: 5,
-        date: "2020/10/04",
-        duration: 18,
-        status: "pending",
-        suggestedActivities: [],
-      },
+    (trips = [{
+      id: 44,
+      userID: 1,
+      destinationID: 49,
+      travelers: 1,
+      date: "2021/09/16",
+      duration: 8,
+      status: "approved",
+      suggestedActivities: [],
+    },
+    {
+      id: 35,
+      userID: 2,
+      destinationID: 25,
+      travelers: 5,
+      date: "2020/10/04",
+      duration: 18,
+      status: "pending",
+      suggestedActivities: [],
+    },
+    {
+      id: 13,
+      userID: 1,
+      destinationID: 22,
+      travelers: 4,
+      date: "2020/05/22",
+      duration: 17,
+      status: "pending",
+      suggestedActivities: [],
+    },
+    {
+      id: 40,
+      userID: 2,
+      destinationID: 14,
+      travelers: 2,
+      date: "2020/02/25",
+      duration: 10,
+      status: "approved",
+      suggestedActivities: [],
+    },
     ]),
-    (destinations = [
-      {
-        id: 1,
-        destination: "Lima, Peru",
-        estimatedLodgingCostPerDay: 70,
-        estimatedFlightCostPerPerson: 400,
-        image:
-            "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-        alt: "overview of city buildings with a clear sky",
-      },
-      {
-        id: 2,
-        destination: "Stockholm, Sweden",
-        estimatedLodgingCostPerDay: 100,
-        estimatedFlightCostPerPerson: 780,
-        image:
-            "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        alt: "city with boats on the water during the day time",
-      },
+    (destinations = [{
+      id: 1,
+      destination: "Lima, Peru",
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 400,
+      image: "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+      alt: "overview of city buildings with a clear sky",
+    },
+    {
+      id: 2,
+      destination: "Stockholm, Sweden",
+      estimatedLodgingCostPerDay: 100,
+      estimatedFlightCostPerPerson: 780,
+      image: "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      alt: "city with boats on the water during the day time",
+    }
     ]),
     (totalSpent1 = 420.69),
-    (totalSpent2 = 694.2),
     (traveler1 = new Traveler(
       travelerID1,
       traveler1Name,
@@ -76,14 +82,6 @@ describe("Traveler", () => {
       trips,
       destinations,
       totalSpent1
-    )),
-    (traveler2 = new Traveler(
-      travelerID2,
-      traveler2Name,
-      traveler2Type,
-      trips,
-      destinations,
-      totalSpent2
     ))
   })
 
@@ -104,24 +102,78 @@ describe("Traveler", () => {
       status: "pending",
       suggestedActivities: [],
     }])
-    expect(result1).to.deep.equal([])
+    expect(result1).to.deep.equal([{
+      id: 40,
+      userID: 2,
+      destinationID: 14,
+      travelers: 2,
+      date: "2020/02/25",
+      duration: 10,
+      status: "approved",
+      suggestedActivities: [],
+    }, ])
   })
 
   it("should get trips by date", () => {
     let result = traveler1.sortTripsByDate("2020/10/04", 2)
     let result1 = traveler1.sortTripsByDate("2020/10/05", 2)
-    expect(result).to.deep.equal([
-      {
-        id: 35,
-        userID: 2,
-        destinationID: 25,
-        travelers: 5,
-        date: "2020/10/04",
-        duration: 18,
-        status: "pending",
-        suggestedActivities: [],
-      },
-    ])
+    expect(result).to.deep.equal([{
+      id: 35,
+      userID: 2,
+      destinationID: 25,
+      travelers: 5,
+      date: "2020/10/04",
+      duration: 18,
+      status: "pending",
+      suggestedActivities: [],
+    }, ])
     expect(result1).to.deep.equal([])
+  })
+
+  it("should catalogue this user's trips by past or future", () => {
+    traveler1.catalogueTrips(1)
+    expect(traveler1.pastTrips).to.deep.equal([{
+      id: 13,
+      userID: 1,
+      destinationID: 22,
+      travelers: 4,
+      date: "2020/05/22",
+      duration: 17,
+      status: "pending",
+      suggestedActivities: [],
+    }, ])
+    expect(traveler1.futureTrips).to.deep.equal([{
+      id: 44,
+      userID: 1,
+      destinationID: 49,
+      travelers: 1,
+      date: "2021/09/16",
+      duration: 8,
+      status: "approved",
+      suggestedActivities: [],
+    }, ])
+  })
+
+  it("should get this user's trips", () => {
+    let result = traveler1.getMyTrips(1)
+    expect(result).to.deep.equal([{
+      id: 44,
+      userID: 1,
+      destinationID: 49,
+      travelers: 1,
+      date: "2021/09/16",
+      duration: 8,
+      status: "approved",
+      suggestedActivities: [],
+    }, {
+      id: 13,
+      userID: 1,
+      destinationID: 22,
+      travelers: 4,
+      date: "2020/05/22",
+      duration: 17,
+      status: "pending",
+      suggestedActivities: [],
+    }])
   })
 })
